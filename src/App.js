c47c83913a8da4d5cd66e3352c611f47169e0198
Vanz-silva <vanz_silva@icloud.com>
2024-08-23 14:41:37 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+import Header from "./componentes/Header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/*Incluir Main*/}
+
+      {/*Incluir Sections*/}
+    </div>
+  );
+}
---
+++ src/componentes/Header.js
@@ -0,0 +1,22 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#sobremim">Sobre Mim</a>
+          </li>
+          <li>
+            <a href="#habilidades">Minhas Habilidades</a>
+          </li>
+          <li>
+            <a href="#projetos">Meus Projetos</a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
